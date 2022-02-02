fb343801dc9da682245c943af0169436e3cec409
David M. Fajardo <davidmf99@gmail.com>
2022-02-02 15:37:22 +0100

    hotfix

--- src/entities/enemies.ts
+++ src/entities/enemies.ts
@@ -7,16 +7,12 @@ export class Slime extends Base_Entity {
 	// easy to kill, but good AP
     constructor(level: number = 1, name: string = 'SLIME', ) {
         super(level, name)
-    }
-
-    _levelUp() {
-        this.level++
-        this.health += base.health - 80
-        this.ad += base.ad
-        this.ap += base.ap + 25
-        this.armor += base.armor - 3
-        this.mr += base.mr
-        this.speed += base.speed - 1
+		// buff
+        this.ap = level * base.ap + 25
+		// nerf
+        this.health = level * base.health - 80
+        this.armor = level * base.armor - 3
+        this.speed = level * base.speed - 1
     }
 }
 
@@ -24,37 +20,28 @@ export class Eagle extends Base_Entity {
 	// easy to kill, but good AD
 	constructor(level: number = 1, name: string = 'EAGLE') {
 		super(level, name)
+		// buff
+        this.ad = level *  base.ad + 15
+        this.speed = level *  base.speed + 4
+		// nerf
+        this.health = level *  base.health - 60
+        this.ap = level *  base.ap - 5
+        this.armor = level *  base.armor - 3
+        this.mr = level *  base.mr - 4
 	}
-
-    _levelUp() {
-        this.level++
-        this.health += base.health - 60
-        this.ad += base.ad + 15
-        this.ap += base.ap - 5
-        this.armor += base.armor - 3
-        this.mr += base.mr - 4
-        this.speed += base.speed + 4
-    }
 }
 
 export class Knight extends Base_Entity {
 	// hard to kill, but low AP/MR
 	constructor(level: number = 1, name: string = 'KNIGHT') {
 		super(level, name)
-		this.armor = level * 8 // buff
-		this.ap = level * 8 // nerf
-		this.mr = level * 3 // nerf
-		this.mr = level * 2 // nerf
+		// buff
+        this.health = level * base.health + 100
+        this.ad = level * base.ad + 5
+        this.armor = level * base.armor + 5
+		// nerf
+        this.ap = level * base.ap - 12
+        this.speed = level * base.speed - 3
 	}
-
-    _levelUp() {
-        this.level++
-        this.health += base.health + 100
-        this.ad += base.ad + 5
-        this.ap += base.ap - 12
-        this.armor += base.armor + 5
-        this.mr += base.mr
-        this.speed += base.speed - 3
-    }
 }
 
